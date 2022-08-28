52d66e7b15aef18b65842c0c9f215be194fcbbe1
Ryan Hofer <r.h.hofer@gmail.com>
2022-08-27 23:51:29 -0500

    chapter 3.2

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::io::{stderr, Write};
+
 fn main() {
     let image_width: i32 = 256;
     let image_height: i32 = 256;
@@ -5,6 +7,9 @@ fn main() {
     println!("P3\n{} {}\n255", image_width, image_height);
 
     for j in (0..image_height).rev() {
+        eprintln!("Scanlines remaining: {} ", j);
+        stderr().flush().expect("failed to flush stderr");
+
         for i in 0..image_width {
             let r = (i as f64) / (image_width - 1) as f64;
             let g = (j as f64) / (image_height - 1) as f64;
@@ -16,5 +21,98 @@ fn main() {
 
             println!("{} {} {}", ir, ig, ib);
         }
+    }
+
+    eprintln!("Done.");
+}
+
+#[derive(Clone, Copy)]
+struct Vec3(f64, f64, f64);
+type Point3 = Vec3;
+type Color = Vec3;
+
+impl Vec3 {
+    fn zero() -> Self {
+        Vec3(0., 0., 0.)
+    }
+
+    fn length(self) -> f64 {
+        self.length_squared().sqrt()
+    }
+
+    fn length_squared(self) -> f64 {
+        self.0 * self.0 + self.1 * self.1 + self.2 * self.2
+    }
+
+    fn dot_product(self, other: Self) -> f64 {
+        self.0 * other.0 + self.1 * other.1 + self.2 * other.2
+    }
+
+    fn cross_product(self, other: Self) -> Self {
+        Self(
+            self.1 * other.2 - self.2 * other.1,
+            self.2 * other.0 - self.0 * other.2,
+            self.0 * other.1 - self.1 * other.0,
+        )
+    }
+
+    fn unit_vector(self) -> Self {
+        self / self.length()
+    }
+}
+
+impl std::ops::Add for Vec3 {
+    type Output = Self;
+
+    fn add(self, other: Self) -> Self::Output {
+        Self(self.0 + other.0, self.1 + other.1, self.2 + other.2)
+    }
+}
+
+impl std::ops::AddAssign for Vec3 {
+    fn add_assign(&mut self, other: Self) {
+        *self = *self + other;
+    }
+}
+
+impl std::ops::Sub for Vec3 {
+    type Output = Self;
+
+    fn sub(self, other: Self) -> Self::Output {
+        Self(self.0 - other.0, self.1 - other.1, self.2 - other.2)
+    }
+}
+
+impl std::ops::SubAssign for Vec3 {
+    fn sub_assign(&mut self, other: Self) {
+        *self = *self - other;
+    }
+}
+
+impl std::ops::Mul<f64> for Vec3 {
+    type Output = Self;
+
+    fn mul(self, rhs: f64) -> Self {
+        Self(self.0 * rhs, self.1 * rhs, self.2 * rhs)
+    }
+}
+
+impl std::ops::MulAssign<f64> for Vec3 {
+    fn mul_assign(&mut self, rhs: f64) {
+        *self = *self * rhs;
+    }
+}
+
+impl std::ops::Div<f64> for Vec3 {
+    type Output = Self;
+
+    fn div(self, rhs: f64) -> Self::Output {
+        self * (1. / rhs)
+    }
+}
+
+impl std::ops::DivAssign<f64> for Vec3 {
+    fn div_assign(&mut self, rhs: f64) {
+        *self = *self / rhs;
     }
 }
