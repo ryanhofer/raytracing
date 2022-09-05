a58894b23e0061165d23b5f8ca1fee535456076d
Ryan Hofer <r.h.hofer@gmail.com>
2022-09-04 20:49:02 -0500

    chapter 7.2

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,20 @@
 use crate::ray::{Hit, Ray, Sphere};
 use crate::vector::{Color, Point3, Vec3};
+use rand::prelude::*;
 use std::io::{stderr, Write};
 
 pub mod ray;
 pub mod vector;
 
 fn main() {
+    let mut rng = thread_rng();
+
     // Image
 
     let aspect_ratio = 16. / 9.;
     let image_width: i32 = 400;
     let image_height: i32 = (image_width as f64 / aspect_ratio) as i32;
+    let samples_per_pixel = 100;
 
     // World
     let world = World {
@@ -21,16 +25,7 @@ fn main() {
     };
 
     // Camera
-
-    let viewport_height = 2.;
-    let viewport_width = aspect_ratio * viewport_height;
-    let focal_length = 1.;
-
-    let origin: Point3 = Vec3::zero();
-    let horizontal = Vec3::new(viewport_width, 0., 0.);
-    let vertical = Vec3::new(0., viewport_height, 0.);
-    let lower_left_corner =
-        origin - horizontal / 2. - vertical / 2. - Vec3::new(0., 0., focal_length);
+    let camera = Camera::new();
 
     // Render
 
@@ -44,12 +39,16 @@ fn main() {
             let j = j as f64;
             let i = i as f64;
 
-            let u = i / (image_width - 1) as f64;
-            let v = j / (image_height - 1) as f64;
-            let direction = lower_left_corner + horizontal * u + vertical * v - origin;
-            let r = Ray::new(origin, direction);
+            let mut pixel_color_accum = Vec3::zero();
+            for _ in 0..samples_per_pixel {
+                let u = (i + rng.gen::<f64>()) / (image_width - 1) as f64;
+                let v = (j + rng.gen::<f64>()) / (image_height - 1) as f64;
+                let r = camera.get_ray(u, v);
 
-            let pixel_color = ray_color(r, &world);
+                pixel_color_accum += ray_color(r, &world);
+            }
+
+            let pixel_color = pixel_color_accum / samples_per_pixel as f64;
             write_color(pixel_color);
         }
     }
@@ -91,5 +90,41 @@ impl Hit for World {
         }
 
         closest_hit
+    }
+}
+
+struct Camera {
+    origin: Point3,
+    lower_left_corner: Point3,
+    horizontal: Vec3,
+    vertical: Vec3,
+}
+
+impl Camera {
+    fn new() -> Self {
+        let aspect_ratio = 16. / 9.;
+        let viewport_height = 2.;
+        let viewport_width = aspect_ratio * viewport_height;
+        let focal_length = 1.;
+
+        let origin = Point3::zero();
+        let horizontal = Vec3::new(viewport_width, 0., 0.);
+        let vertical = Vec3::new(0., viewport_height, 0.);
+        let lower_left_corner =
+            origin - horizontal / 2. - vertical / 2. - Vec3::new(0., 0., focal_length);
+
+        Self {
+            origin,
+            lower_left_corner,
+            horizontal,
+            vertical,
+        }
+    }
+
+    fn get_ray(&self, u: f64, v: f64) -> Ray {
+        Ray::new(
+            self.origin,
+            self.lower_left_corner + self.horizontal * u + self.vertical * v - self.origin,
+        )
     }
 }
